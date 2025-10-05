f4c54aee9cdb59a0b72d2ab45003967c73eb839a
romankr <romankr@default-value.com>
2025-10-05 19:19:36 +0300

    Implement falling snow

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module falling-sand
+module falling-snow
 
 go 1.25.1
 
--- main.go
+++ main.go
@@ -1,46 +1,78 @@
 package main
 
-import "github.com/gen2brain/raylib-go/raylib"
+import (
+	"github.com/gen2brain/raylib-go/raylib"
+	"math/rand"
+)
 
-const FontSize = 18
-const Scale = 1.5
-const ParticalRadius = 4
-const ParticalSpeed = 2
+const WindowWidth = 800
+const WindowHeight = 600
+const InitAmount = 800
+const SpawnRate = 2
 
 type Point struct {
-	X int32
-	Y int32
+	x      float32
+	y      float32
+	speed  float32
+	radius float32
+}
+
+func newPoint(x, y float32) Point {
+	var speed float32 = rand.Float32() + 1
+	var radius float32 = speed * 0.5
+	return Point{
+		x:      x,
+		y:      y,
+		speed:  speed,
+		radius: radius,
+	}
+}
+
+func newTopPoint() Point {
+	x := rand.Int31n(WindowWidth)
+	return newPoint(float32(x), 0)
+}
+
+func newInitPoint() Point {
+	x := rand.Int31n(WindowWidth)
+	y := rand.Int31n(WindowHeight)
+	return newPoint(float32(x), float32(y))
 }
 
 func main() {
 	rl.SetConfigFlags(rl.FlagWindowHighdpi)
-
-	rl.InitWindow(800, 600, "Falling Sand")
-	defer rl.CloseWindow()
-
+	rl.SetTraceLogLevel(rl.LogNone)
 	rl.SetTargetFPS(60)
 
+	BackgroundColor := rl.NewColor(10, 11, 39, 255)
+	rl.InitWindow(WindowWidth, WindowHeight, "Falling Snow")
+	defer rl.CloseWindow()
+
 	var points []Point
+	for range InitAmount {
+		points = append(points, newInitPoint())
+	}
 
 	for !rl.WindowShouldClose() {
-		mouseX := int32(float32(rl.GetMouseX()) * Scale)
-		mouseY := int32(float32(rl.GetMouseY()) * Scale)
-
-		if rl.IsMouseButtonDown(rl.MouseButtonLeft) {
-			points = append(points, Point{mouseX, mouseY})
+		for range SpawnRate {
+			points = append(points, newTopPoint())
 		}
 
 		rl.BeginDrawing()
-		rl.ClearBackground(rl.Black)
+		rl.ClearBackground(BackgroundColor)
 
-		rl.DrawCircle(mouseX, mouseY, ParticalRadius, rl.RayWhite)
-
-		for i, point := range points {
-			rl.DrawCircle(point.X, point.Y, ParticalRadius, rl.RayWhite)
-			if points[i].Y < 400 {
-				points[i].Y = point.Y + ParticalSpeed
-			}
+		for _, point := range points {
+			rl.DrawCircle(int32(point.x), int32(point.y), point.radius, rl.RayWhite)
 		}
 		rl.EndDrawing()
+
+		var next []Point
+		for i, point := range points {
+			if points[i].y < WindowHeight {
+				point.y = point.y + point.speed
+				next = append(next, point)
+			}
+		}
+		points = next
 	}
 }
